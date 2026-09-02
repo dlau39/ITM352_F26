b31b9611c29444672194105cdf53b1f64751c845
Dominic Lau <meh054111@gmail.com>
2026-09-02 13:13:10 -1000

    Created Ex3 and Ex4

---
+++ Lab2/Ex3.py
@@ -0,0 +1,13 @@
+# This program prompts the user to enter a number or decimal between 1 and 100 
+# then prints the number and its square.
+# Name: Dominic Lau
+# Date: Sept. 2, 2026
+
+print('Enter a number or decimal between 1 - 100:')
+value_entered = float(input())
+print('The value that you entered is', value_entered)
+# This code takes a floating point number input from the user and prints it out.
+print('The square of that number is', value_entered ** 2)
+# This code squares the floating point number entered by the user and prints it out.
+
+print(f'The value that you entered is {value_entered} and the square of that number is {value_entered ** 2}')
---
+++ Lab2/Ex4.py
@@ -0,0 +1,17 @@
+# This program prompts the user to enter a number or decimal between 1 and 100 
+# then round the number to two decimal places and prints the number and its square.
+# Name: Dominic Lau
+# Date: Sept. 2, 2026
+
+print('Enter a number or decimal between 1 - 100:')
+value_entered = float(input())
+print('The value that you entered is', value_entered)
+# This code takes a floating point number input from the user and prints it out.
+
+valueSquared = value_entered ** 2
+
+roundedValue = round(value_entered, 2)
+print('The square of that number is', round(valueSquared, 2))
+# This code squares the floating point number entered by the user and prints it out.
+
+print(f'The value that you entered is {roundedValue} and the square of that number is {round(valueSquared, 2)}')
